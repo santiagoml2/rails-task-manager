7515530d05a413a88c525691022347340506a1ba
santiago munoz <santiagoml2@hotmail.com>
2022-09-10 13:56:28 -0500

    routes have been created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # READ
+  get 'tasks', to: 'tasks#index'
+  # CREATE
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'task#create'
+  # READ
+  get 'taks/:id', to: 'tasks#show', as: :task
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
